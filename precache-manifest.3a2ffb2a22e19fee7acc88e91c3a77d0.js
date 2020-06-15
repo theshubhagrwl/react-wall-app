aa61fb7024fa7ca572cfd9fcbbb9572a000a6025
Shubh Agrawal <shubhagrawal1280@gmail.com>
2020-06-15 10:25:02 +0530

    Updates

---
+++ precache-manifest.3a2ffb2a22e19fee7acc88e91c3a77d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ac9a569a123fba50e237917742b883",
+    "url": "/react-wall-app/index.html"
+  },
+  {
+    "revision": "dfc95d8ad7c5ad2d339d",
+    "url": "/react-wall-app/static/css/main.fd46c46c.chunk.css"
+  },
+  {
+    "revision": "fbf3885bd4248afc3f03",
+    "url": "/react-wall-app/static/js/2.6f29fa35.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-wall-app/static/js/2.6f29fa35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc95d8ad7c5ad2d339d",
+    "url": "/react-wall-app/static/js/main.47cabd78.chunk.js"
+  },
+  {
+    "revision": "27ea741050caaff48711",
+    "url": "/react-wall-app/static/js/runtime-main.c6eb9116.js"
+  }
+]);
