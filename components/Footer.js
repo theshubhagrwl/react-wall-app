45f64c22106c052ce73e5f90848fd84e03a99761
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-09-09 08:25:39 +0530

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,12 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="h-10 text-center">
-      Made with <span className="text-red-500">❤</span>
-      by{" "}
-      <a
-        href="https://theshubhagrwl.vercel.app/"
-        className="text-purple-600 font-bold"
-      >
+      Made with <span className="text-red-500">❤</span> by{" "}
+      <a href="https://theshubhagrwl.in/" className="text-purple-600 font-bold">
         Shubh
       </a>
     </footer>
