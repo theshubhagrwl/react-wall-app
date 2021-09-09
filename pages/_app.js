959e1aa41851be73e78e4accefbb590dccc10239
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-09-09 08:25:27 +0530

    added loading on routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,34 @@
+import React, { useEffect } from "react";
 import Head from "next/head";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`);
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routerChangeStart", handleStart);
+      router.events.off("routerChangeComplete", handleStop);
+      router.events.off("routerChangeError", handleStop);
+    };
+  }, [router]);
+
   return (
     <>
       <Head>
